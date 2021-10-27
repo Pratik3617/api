2cbcda848cc82751b09829f7252f1907c050ad55
Pratik <pp4112030@gmail.com>
2021-10-27 12:32:02 +0530

    added some corrections

--- api/view.py
+++ api/view.py
@@ -4,10 +4,10 @@ from django.http import JsonResponse
 
 def show(request):
     value = request.GET['Interest']
-    print(value)
+    # print(value)
     data={'ticker':value}
     row=requests.post("https://docker.api.srifintech.com/testassignment",data=data).json()
-    print(row)
+    # print(row)
     return JsonResponse(row,safe=False)
 
 def IndexInterface(request):
