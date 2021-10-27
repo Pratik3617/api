68272feca91eb9fd6d3e57493d6712d595d42bd8
Pratik <pp4112030@gmail.com>
2021-10-27 11:00:28 +0530

    fetched data

---
+++ api/view.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+
+def show(request):
+    value = request.GET['Interest']
+    print(value)
+    data={'ticker':value}
+    row=requests.post("https://docker.api.srifintech.com/testassignment",data=data).json()
+    print(row)
+    return JsonResponse(row,safe=False)
+
+def IndexInterface(request):
+    return render(request,'index.html')
+
